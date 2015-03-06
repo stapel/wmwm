e47c0726734b3401ae0e969f0e447cae530541bd
Andreas Müller <andreas@stapelspeicher.org>
2015-03-06 04:17:38 +0100

    hidden.c: Add not that this does not work anymore due to reparenting

--- hidden.c
+++ hidden.c
@@ -17,6 +17,11 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+
+/* XXX
+ * Does not work anymore because this is a reparenting WM now!
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>

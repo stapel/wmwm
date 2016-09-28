25d377dfd07b6ce03e5a7fd9be2568f7acbd6155
Andreas Müller <andreas@stapelspeicher>
2016-09-28 21:15:01 +0200

    tree.h window_tree.h: cleanup

--- tree.h
+++ tree.h
@@ -3,14 +3,12 @@
 
 /* n-ary Tree implementation
  *
- * Each node has exactly one parent, two siblings (prev, next), one child
+ * Each node has one parent, two siblings (prev, next), one child
  * and a pointer to arbitrary data.
  *
  * child is always the first (prev == NULL) in the list of siblings
  */
-
 typedef struct tree_item tree_t;
-
 struct tree_item {
 	void   *data;   /* arbitary data */
 
@@ -20,25 +18,22 @@ struct tree_item {
 	tree_t *child;  /* child */ /* only for workspace and tiling! */
 };
 
-
-// helper functions
+/* helper functions */
 static tree_t *tree_parent(tree_t *node) { return node->parent; }
 static tree_t *tree_child(tree_t *node) { return node->child; }
 static void *tree_data(tree_t *node) { return node->data; }
 
-// create new node
+/* create new node */
 tree_t *tree_new(tree_t *parent, tree_t *prev, tree_t *next, tree_t *child,
 		void *data);
 
-// remove node from tree, fix ancestors, take child with it
+/* remove node from tree, fix ancestors, take child with it */
 void tree_extract(tree_t *node);
-
-// insert _node_ as sibling before _old_
+/* insert _node_ as sibling before _old_ */
 void tree_insert(tree_t *next, tree_t *node);
-
-void tree_replace(tree_t *from, tree_t *to);
-
-// insert _node_ as sibling after _old_
+/* insert _node_ as sibling after _old_ */
 void tree_add(tree_t *next, tree_t *node);
+/* replace _from_ with _to_ */
+void tree_replace(tree_t *from, tree_t *to);
 
 #endif /* __WMWM__TREE_H__ */
--- window_tree.h
+++ window_tree.h
@@ -1,6 +1,5 @@
 #ifndef __WMWM__CONTAINER_TREE_H__
 #define __WMWM__CONTAINER_TREE_H__
-
 #include <stdint.h>   // for uint16_t
 #include "stdbool.h"  // for bool
 #include "tree.h"     // for tree_t
@@ -15,87 +14,70 @@ typedef enum tiling_modes {
 	TILING_FLOATING
 } tiling_t;
 
-
-
-/* dumb alias */
-
 /* general container types */
-/****************************************************************/
-/* Possible container types */
 typedef enum container_types {
 	CONTAINER_TILING,
 	CONTAINER_CLIENT
 } container_type;
 
-
-
-// XXX tiling: fix union and stuff
-/* container */
+/* container * static local helper functions in window_tree.c */
 typedef struct container {
 	container_type type;
 	union {
 		struct {
 			tiling_t tile;
-			uint16_t  tiles;
-			uint16_t x_scale;
-			uint16_t y_scale;
+			uint16_t tiles;
 		};
-		client_t    *client;
+		client_t *client;
 	};
 } container_t;
-
-/* client-wtree-node *****************
-   - MUST NOT HAVE child
-   - MUST HAVE a parent
-   - MAY HAVE siblings
-
- * tiling-wtree-node *****************
-
-   if mode == floating
-   - MUST HAVE client-wtree-node child
-   - MUST HAVE a parent (except root)
-
-   if mode == tiling
-   - MUST HAVE children
-   - MUST HAVE a parent if not root-node
-   - MAY HAVE siblings
-
-*/
-
-
 
 /* create new node with client/tiling "container" */
 wtree_t* wtree_new_client(client_t *client);
 wtree_t* wtree_new_tiling(tiling_t tile);
+
 /* free node and its data */
 void wtree_free(wtree_t *node);
 
-
+/* get client from node */
 client_t *wtree_client(wtree_t *node);
 
+/* check if node is ... */
 bool wtree_is_client(wtree_t *node);
 bool wtree_is_tiling(wtree_t *node);
 bool wtree_is_singleton(wtree_t* child);
 
+/* get number of tiles/children */
 uint16_t wtree_get_tiles(wtree_t *node);
+
+/* get tiling_t of node/parent */
 tiling_t wtree_tiling(wtree_t *node);
 tiling_t wtree_parent_tiling(wtree_t *node);
+
+/* set tiling_t of node/parent */
 void wtree_set_tiling(wtree_t *node, tiling_t tiling);
 void wtree_set_parent_tiling(wtree_t *node, tiling_t tiling);
 
+/* add/append sibling/children */
 void wtree_add_sibling(wtree_t *current, wtree_t *node);
 void wtree_append_sibling(wtree_t *current, wtree_t *node);
 void wtree_append_child(wtree_t *parent, wtree_t *node);
+
+/* put new tiling node between client and client->parent */
 void wtree_inter_tile(wtree_t *client, tiling_t mode);
-void wtree_add_tile_sibling(wtree_t *current, wtree_t *node,
-		tiling_t tiling);
+
+/* add tiling-node as sibling to current and client-node as child to tiling-node */
+void wtree_add_tile_sibling(wtree_t *current, wtree_t *node, tiling_t tiling);
+
+/* unlink node from tree, fix siblings and parent */
 void wtree_remove(wtree_t *node);
 
+/* for each client-node below node, do action(client), pre-order*/
 void wtree_traverse_clients(wtree_t *node, void(*action)(client_t *));
+/* find node below _node_ that has compare(client) == true, pre-order */
 client_t *wtree_find_client(wtree_t *node, bool(*compare)(client_t*, void *), void *arg);
 
-/* print tree */
+/* print tree to dot file */
 void wtree_print_tree(wtree_t *cur);
-
 
 #endif
